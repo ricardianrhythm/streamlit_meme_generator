6d48d9374fbcb5b5cc7e54acbe8403edb91bb428
ricardianrhythm <richardleewei@gmail.com>
2024-09-16 23:12:20 -0700

    Update gradio_meme_generator_20240916.py

--- gradio_meme_generator_20240916.py
+++ gradio_meme_generator_20240916.py
@@ -12,10 +12,8 @@ from tenacity import retry, stop_after_attempt, wait_exponential
 import os
 import random
 
-firebase_credentials = st.secrets["firebase"]
-
 # Initialize Firebase (replace with your own credentials)
-cred = credentials.Certificate(firebase_credentials)
+cred = credentials.Certificate(st.secrets["firebase"])
 firebase_admin.initialize_app(cred)
 db = firestore.client()
 
